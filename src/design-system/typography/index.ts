4584f43a879bf806984e94439473d463e4140692
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2021-03-13 10:52:26 +0100

    Adjust typography

--- src/design-system/typography/index.ts
+++ src/design-system/typography/index.ts
@@ -1,6 +1,10 @@
 import Typography from 'typography'
 import moragaTheme from 'typography-theme-moraga'
 
-const typography = new Typography(moragaTheme)
+const typography = new Typography({
+  ...moragaTheme,
+  baseFontSize: '15px',
+  baseLineHeight: 1.2,
+})
 export const { scale, rhythm, options } = typography
 export default typography
