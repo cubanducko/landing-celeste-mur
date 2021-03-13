59b248e3b5c6433bcc6c710b525bc4c1186b6579
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2021-03-13 09:51:53 +0100

    Add work page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { ProjectPreview, ProjectPreviewData } from 'components/Project'
+import { graphql } from 'gatsby'
 import React from 'react'
 
-export default function Home() {
-  return <div>La famosa ilustradora, Celeste Mur</div>
+export default function Home({ data }) {
+  const projects = data.allContentfulProject.edges
+  console.log(data)
+  return (
+    <section>
+      {projects.map(({ node }: { node: ProjectPreviewData }) => (
+        <ProjectPreview key={node.slug} {...node} />
+      ))}
+    </section>
+  )
 }
+
+export const query = graphql`
+  query HomePageQuery($locale: String) {
+    allContentfulProject(filter: { node_locale: { eq: $locale } }) {
+      edges {
+        node {
+          slug
+          title
+          previewImage {
+            fluid(maxWidth: 360, maxHeight: 240) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
