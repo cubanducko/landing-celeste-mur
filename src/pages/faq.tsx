541110ebce99b273439bb669343b408b9f8a3f60
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2021-03-13 09:51:39 +0100

    Add FAQ page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,5 +1,54 @@
+import { graphql } from 'gatsby'
 import React from 'react'
+import Img from 'gatsby-image'
+import { Question, QuestionProps } from 'components/Question'
+import { css, makeStyles } from 'services/styles'
+import { Theme } from 'design-system'
 
-export default function FAQ() {
-  return <div>La famosa ilustradora, Celeste Mur</div>
+export default function FAQ({ data }) {
+  const { title, questions, faqDetail } = data.contentfulFaq
+  const classes = useStyles()
+  return (
+    <section className={classes.container}>
+      <aside className={classes.imageWrapper}>
+        <Img {...faqDetail} />
+      </aside>
+      <article>
+        <h1>{title}</h1>
+        {questions.map((question: Pick<QuestionProps, 'answer' | 'question'>) => (
+          <Question key={question.question} {...question} />
+        ))}
+      </article>
+    </section>
+  )
 }
+
+export const query = graphql`
+  query FAQPageQuery($locale: String) {
+    contentfulFaq(node_locale: { eq: $locale }) {
+      faqDetail {
+        fluid(maxWidth: 500) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+      questions {
+        answer {
+          raw
+        }
+        question
+      }
+      title
+    }
+  }
+`
+
+const useStyles = makeStyles(({ spacing, breakpoints }: Theme) => ({
+  container: css`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: ${spacing(4)};
+  `,
+  imageWrapper: css`
+    width: 100%;
+  `,
+}))
