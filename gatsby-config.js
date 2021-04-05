61e63af70bd80764941c4f34939526fb7304fb92
Ricardo Domínguez Álvarez <r.dominguez.alvarez.dev@gmail.com>
2021-04-05 16:32:51 +0200

    Upload new host .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ if (process.env.ENVIROMENT !== 'production') {
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_HOST,
 }
 
 module.exports = {
